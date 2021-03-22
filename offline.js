014fff0a59263d634328554fa780b68e25c73cb6
guruchian <30399464+semicon@users.noreply.github.com>
2021-03-22 23:39:20 +0700

    math24game

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1616430435,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/box-sheet0.png",
+		"images/box-sheet1.png",
+		"images/sign-sheet0.png",
+		"images/sign-sheet1.png",
+		"images/setangle-sheet0.png",
+		"images/collitionbox-sheet0.png",
+		"images/particles.png",
+		"images/replay-sheet0.png",
+		"images/newrand-sheet0.png",
+		"images/base-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
